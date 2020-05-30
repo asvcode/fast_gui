63f4a91a58854017518fc1ed97cd29e87b415ff2
asvcode <amrit@pillview.com>
2020-05-30 15:21:35 -0700

    updates

--- visualtemp/_nbdev.py
+++ visualtemp/_nbdev.py
@@ -2,9 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"dashboard_one": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://asvcode.github.io/visualtemp/"
 
